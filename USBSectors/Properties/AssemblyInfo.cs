1d94073a615ff267258033caa41fd69a613f0062
Garmatin Vyacheslav <vyacheslav.garmatin@sprinterra.com>
2019-04-23 13:44:00 +0300

    Added assemblyInfo

--- USBSectors/Properties/AssemblyInfo.cs
+++ USBSectors/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Win32")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("USBSectors")]
+[assembly: AssemblyDescription("Library for operating raw sectos of USB flash drive")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Win32")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("hungry_doctor")]
+[assembly: AssemblyProduct("USBSectors library")]
+[assembly: AssemblyCopyright("Copyright ©  2019 by hungry_doctor")]
+[assembly: AssemblyTrademark("No")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
